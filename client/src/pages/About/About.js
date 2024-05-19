9b3893d6f06486748709d7af7247cb0aeb058507
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-05-19 13:50:26 +0530

    update

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -21,7 +21,7 @@ const About = () => {
                     I Have Completed My Bachlor's Degree In Computer Science From Patliputra University, Bihar.
                     I Am Pursuing Master Of Computer Application (MCA) From a Galgotias University In Greater noida.
                     My Hobbies are I like Lerning new Things and Having a Great Passion For Technology.
-                    I Have Deep Interest In Devlopment and I have made Many FullStack Project Using HTML, CSS, Javascript, React.js, Node.js, Express.js, Mongoose, Mongodb, tailwind.
+                    I Have Deep Interest In Devlopment and I have made Many FullStack Project Using HTML, CSS, Javascript, React.js, Node.js, Express.js, Mongoose, Mongodb, tailwind, Django and Next.js.
                     My Short Term Goal is to get a job in Reputed Company and My long Term Goal is to a Achive a Higher Position in the same Company.
                     That all About Me.
               </p>
